82aedb3c24df24847327363ebdf063ea041deaea
Anushk Mittal <anushkmittal@icloud.com>
2018-03-19 00:14:06 -0400

    this just works

--- Server/index.js
+++ Server/index.js
@@ -5,24 +5,10 @@ var dynamodb = new doc.DynamoDB();
 exports.handler = function(event, context) {
     console.log("Request received:\n", JSON.stringify(event));
     console.log("Context received:\n", JSON.stringify(context));
-
     var tableName = "use-logger";
-    var item = {
-        "meta": event.meta,
-        "sno":  event.sno,
-        "what": event.what,
-        "when": event.when,
-        "where": {
-            "location": event.location,
-            "streetAddress": event.streetAddress
-        },
-        "who": event.who
-    };
-    console.log("Item:\n", item);
-
     dynamodb.putItem({
             "TableName": tableName,
-            "Item": item
+            "Item": event
         }, function(err, data) {
             if (err) {
                 context.fail('ERROR: Dynamo failed: ' + err);
