36f0724e5ad995a552b0c1a98e34fa3355f6a0f5
Jayanth M <jaym93@gmail.com>
2018-03-21 23:01:43 -0400

    Add examples of Slack webhook integrations to *Slack hooks* folder. File `tokens.py` needs to be updated with keys, intent_parser can be replaced with api.ai or wit.ai, and Slackbot.py is the main file that runs as a server (even on localhost)

---
+++ Slack hooks/Slackbot.py
@@ -0,0 +1,5 @@
+# File: Slackbot 
+# Author: Jayanth M (jayanth6@gatech.edu)
+# Created: 3/21/2018 10:54 PM 
+# Project: ctxmgmt
+# Description: 
---
+++ Slack hooks/intent_parser.py
@@ -0,0 +1,5 @@
+# File: intent_parser 
+# Author: Jayanth M (jayanth6@gatech.edu)
+# Created: 3/21/2018 10:57 PM 
+# Project: ctxmgmt
+# Description: 
