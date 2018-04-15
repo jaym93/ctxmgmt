1e9f6a2c8f75a59dcf443a84f80031957d33861a
Jayanth M <jaym93@gmail.com>
2018-04-15 21:48:11 +0000

    Sending data to AWS

--- ctxmgmt/Read.py
+++ ctxmgmt/Read.py
@@ -6,9 +6,14 @@ import SimpleMFRC522
 from gyro import get_magnitude
 from location import get_location
 import re
+import datetime
 import time
+import psutil
+import urllib2
+import requests
+import json
 
-device_name = "rpi2"
+device_name = "device one"
 
 reader = SimpleMFRC522.SimpleMFRC522()
 while True:
@@ -18,16 +23,19 @@ while True:
         new_id,text = reader.read()
         packet['what'] = device_name
         packet['who_id'] = new_id
-        packet['who'] = text
-        location = get_location(device_name)
+        packet['who'] = text.strip()
+        location = get_location('rpi')
         if location['message'] == 'ok':
             packet['where'] = location['loc']
         else:
             packet['where'] = "unknown location"
-        packet['when'] = time.time()
+        packet['sno'] = str(time.time())
+        packet['when'] = time.strftime('%m/%d/%Y %I:%M:%S %p')
+        packet['meta'] = {'cpu': str(psutil.cpu_percent())+'%', 'memory':str(psutil.virtual_memory().percent)+'%', 'disk':str(psutil.disk_usage('/').percent)+'%'}
         print(str(packet))
-    else:
-        pass
-        # print("not moved")
+        aws_url = 'https://ao9kkk5kze.execute-api.us-east-1.amazonaws.com/testing/'
+        head = {'Content-Type':'application/json'}
+        r = requests.post(aws_url, json.dumps(packet), headers=head)
+        print('RESP '+str(r.json()))
     
 GPIO.cleanup()
